74003701d3a719efa9bed190a5a5c97e5d526c78
evgkaptsov <evgkaptsov@gmail.com>
2026-04-03 02:31:54 +0700

    refactoring + colors and fonts are adjusted

--- proj01/maze_SDT.py
+++ proj01/maze_SDT.py
@@ -28,11 +28,13 @@ Note that this code is intended for educational purposes. It is deliberately sim
 """
 
 
-# include standard PLY Lex package
+# import standard PLY Lex package
 import ply.lex as lex
+# import tkinter for drawing
 import tkinter as tk
 from pyrect import Rect
 from collections import deque
+from math import ceil
 
 
 tokens = [
@@ -69,14 +71,11 @@ def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
     
+
+class BasicSDT:
     
-class MazeSDT:
-    
-    WALL_COLOR = "#aaaaaa"
-    
-    def __init__(self, tkRoot):
+    def __init__(self):
         self.tokens = deque()
-        self.tkRoot = tkRoot
         
     def nextToken(self):
         return self.tokens.popleft()
@@ -90,18 +89,53 @@ class MazeSDT:
         
     def matchNext(self, tokenType):
         self.match(self.nextToken(), tokenType)
-    
-    def parse(self, input):
         
+    def parse(self, input):
+            
         lexer = lex.lex()
         lexer.input(input)
         self.tokens = deque()
         
         for tok in lexer:
             self.tokens.append(tok)
-        
-        self.parse_Level()
             
+            
+class MazeStyles:
+    
+    VIEWPORT_OFFSET = 25
+    
+    MAZE_BG_COLOR = "#ddddbb"
+    VIEWPORT_BG_COLOR = "#330000"
+    WALL_COLOR = "#777777"
+    OBJECT_OUTLINE_COLOR = "#444444"
+    
+    INNER_WALL_WIDTH = 4
+    OUTER_WALL_WIDTH = 8
+    DOOR_SIZE_MULT = 0.12
+    
+    OBJECT_SIZE = 25
+    OBJECT_OFFSET = 10
+    
+    OBJ_COIN_COLOR = "#f7d42d"
+    OBJ_HERO_COLOR = "#00ff00"
+    OBJ_ENEMY_COLOR = "#cc0000"
+    OBJ_PORTAL_COLOR = "cyan"
+    OBJ_GOAL_COLOR = "#9b1fe8"
+    
+
+    
+
+class MazeSDT(BasicSDT):
+    
+    
+    def __init__(self, tkRoot):
+        super().__init__()
+        self.tkRoot = tkRoot
+        
+    def parse(self, input):
+        super().parse(input)
+        self.parse_Level()
+
     def parse_Level(self):
         rect = self.parse_Size()
         self.parse_Room(rect)
@@ -114,21 +148,23 @@ class MazeSDT:
         
         # action
         size = int(tok.value)
-        self.size = size
         
-        offset = 25
+        offset = MazeStyles.VIEWPORT_OFFSET
         self.canvas = tk.Canvas(self.tkRoot, width=size+2*offset, height=size+2*offset)
         self.canvas.pack()
         
         rect = Rect(offset, offset, size, size)
-        self.canvas.config(bg="black")
+        self.canvas.config(bg=MazeStyles.VIEWPORT_BG_COLOR)
         self.canvas.create_rectangle(
             rect.left, 
             rect.top, 
             rect.right, 
-            rect.bottom, fill="white", outline=MazeSDT.WALL_COLOR, width=8)
+            rect.bottom, 
+            fill=MazeStyles.MAZE_BG_COLOR, 
+            outline=MazeStyles.WALL_COLOR, 
+            width=MazeStyles.OUTER_WALL_WIDTH)
         
-        print(f"Level size: {self.size} x {self.size}." )
+        print(f"Level size: {size} x {size}." )
         # /action
         
         return rect
@@ -167,34 +203,37 @@ class MazeSDT:
         print(f"Door: {tok.value}")
         
         # action
-        hw = rect.width * 0.12
-        hh = rect.height * 0.12
-        params = dict(width=8, fill="white")
+        hw = rect.width * MazeStyles.DOOR_SIZE_MULT
+        hh = rect.height * MazeStyles.DOOR_SIZE_MULT
+        w = 2*MazeStyles.INNER_WALL_WIDTH + 1
         
-        if tok.value == "l":
-            self.canvas.create_line(
-                rect.left, 
-                rect.centery - hh, 
-                rect.left, 
-                rect.centery + hh, **params)
-        elif tok.value == "r":
-            self.canvas.create_line(
-                rect.right, 
-                rect.centery - hh, 
-                rect.right, 
-                rect.centery + hh, **params)
-        elif tok.value == "t":
-            self.canvas.create_line(
-                rect.centerx - hw, 
-                rect.top, 
-                rect.centerx + hw, 
-                rect.top, **params)
-        else: # b
-            self.canvas.create_line(
-                rect.centerx - hw, 
-                rect.bottom, 
-                rect.centerx + hw, 
-                rect.bottom, **params)
+        params = dict(width=w, fill=MazeStyles.MAZE_BG_COLOR)
+        
+        match tok.value:
+            case "l":
+                self.canvas.create_line(
+                    rect.left, 
+                    rect.centery - hh, 
+                    rect.left, 
+                    rect.centery + hh, **params)
+            case "r":
+                self.canvas.create_line(
+                    rect.right, 
+                    rect.centery - hh, 
+                    rect.right, 
+                    rect.centery + hh, **params)
+            case "t":
+                self.canvas.create_line(
+                    rect.centerx - hw, 
+                    rect.top, 
+                    rect.centerx + hw, 
+                    rect.top, **params)
+            case "b":
+                self.canvas.create_line(
+                    rect.centerx - hw, 
+                    rect.bottom, 
+                    rect.centerx + hw, 
+                    rect.bottom, **params)
         # /action
         
         self.match(tok, 'DOOR')
@@ -203,7 +242,7 @@ class MazeSDT:
         print(f"Div: {tok.value}")
         
         # action
-        wall_width = 4
+        wall_width = MazeStyles.INNER_WALL_WIDTH
         if tok.value == 'v': #vert
             hw = rect.centerx - rect.left
             rect1 = Rect(rect.left, rect.top, hw, rect.height)
@@ -212,7 +251,7 @@ class MazeSDT:
                 rect.centerx, 
                 rect.top, 
                 rect.centerx, 
-                rect.bottom, fill=MazeSDT.WALL_COLOR, width=wall_width)
+                rect.bottom, fill=MazeStyles.WALL_COLOR, width=wall_width)
         else: # horiz
             hh = rect.centery - rect.top
             rect1 = Rect(rect.left, rect.top, rect.width, hh)
@@ -221,7 +260,7 @@ class MazeSDT:
                 rect.left, 
                 rect.centery, 
                 rect.right, 
-                rect.centery, fill=MazeSDT.WALL_COLOR, width=wall_width)
+                rect.centery, fill=MazeStyles.WALL_COLOR, width=wall_width)
         # /action
         
         self.match(tok, 'DIV')
@@ -242,34 +281,42 @@ class MazeSDT:
         
         # action
         
-        objSize = 25
-        objOffset = 10
+        outl = MazeStyles.OBJECT_OUTLINE_COLOR
+        objSize = MazeStyles.OBJECT_SIZE
+        objOffset = MazeStyles.OBJECT_OFFSET
         obj_l = rect.left + n * objSize + (n+1) * objOffset
         obj_t = rect.top + n * objSize + (n+1) * objOffset
         obj_r = obj_l + objSize
         obj_b = obj_t + objSize
+        obj_cx = (obj_l + obj_r)/2
+        obj_cy = (obj_t + obj_b)/2
+        fnt = ("Arial", ceil(objSize/2))
         
-        if tok.value == "coin":
-            self.canvas.create_oval(obj_l, obj_t, obj_r, obj_b, fill="yellow", outline="black")
-        elif tok.value == "enemy":
-            self.canvas.create_oval(obj_l, obj_t, obj_r, obj_b, fill="red", outline="black")
-        elif tok.value == "portal":
-            self.canvas.create_rectangle(obj_l, obj_t, obj_r, obj_b, fill="cyan", outline="black")
-        elif tok.value == "hero":
-            self.canvas.create_polygon([
-                    obj_l + 0.5 * objSize, obj_t, 
-                    obj_l + objSize, obj_b,
-                    obj_l, obj_b,
-                ], 
-                fill="green", outline="black")
-        elif tok.value == "goal":
-            self.canvas.create_polygon([
-                    obj_l, obj_t, 
-                    obj_r, 0.5 * (obj_t + obj_b),
-                    obj_l, obj_b
-                ], 
-                fill="red", outline="black")
-            self.canvas.create_line(obj_l, obj_t, obj_l, obj_b + objSize*0.5)
+        match tok.value:
+            case "coin":
+                self.canvas.create_oval(obj_l, obj_t, obj_r, obj_b, fill=MazeStyles.OBJ_COIN_COLOR, outline=outl)
+                self.canvas.create_text(obj_cx, obj_cy, text="$", font=fnt)
+            case "enemy":
+                self.canvas.create_rectangle(obj_l, obj_t, obj_r, obj_b, fill=MazeStyles.OBJ_ENEMY_COLOR, outline=outl)
+                self.canvas.create_text(obj_cx, obj_cy, text="^^", font=fnt, fill="white")
+            case "portal":
+                self.canvas.create_oval(obj_l, obj_t, obj_r, obj_b, fill=MazeStyles.OBJ_PORTAL_COLOR, outline=outl)
+                self.canvas.create_text(obj_cx, obj_cy, text="@", font=fnt, fill=MazeStyles.OBJECT_OUTLINE_COLOR)
+            case "hero":
+                self.canvas.create_polygon([
+                        obj_l + 0.5 * objSize, obj_t, 
+                        obj_l + objSize, obj_b,
+                        obj_l, obj_b,
+                    ], 
+                    fill=MazeStyles.OBJ_HERO_COLOR, outline=outl)
+            case "goal":
+                self.canvas.create_polygon([
+                        obj_l, obj_t, 
+                        obj_r, 0.5 * (obj_t + obj_b),
+                        obj_l, obj_b
+                    ], 
+                    fill=MazeStyles.OBJ_GOAL_COLOR, outline=outl)
+                self.canvas.create_line(obj_l, obj_t, obj_l, obj_b + objSize*0.5)
             
         # /action
         
@@ -283,14 +330,14 @@ def main():
     levelDescription = '''500
     [v
          [h
-          [{r}{coin}]
+          [{r}{coin coin coin}]
           [{t}{hero portal}]
          ]
         [v
          [{}{portal}]
          [h
-              [{l}{coin coin enemy}]
-              [{t}{enemy goal}]
+              [{l}{coin enemy coin}]
+              [{t}{enemy goal coin}]
          ]
         ]
     ]
@@ -298,7 +345,7 @@ def main():
     
     
     tkRoot = tk.Tk()
-    tkRoot.title("Maze (SCI19 3211)")
+    tkRoot.title("Maze Renderer (SCI19 3211)")
     
     maze = MazeSDT(tkRoot)
     maze.parse(levelDescription)
